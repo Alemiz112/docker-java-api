718984e64dfc8853982f59a52d6253acd9dd799a
Alemiz112 <alex@mizerak.eu>
2021-11-02 17:14:01 +0100

    Disable version stuff in header

--- src/main/java/com/amihaiemil/docker/UserAgentRequestHeader.java
+++ src/main/java/com/amihaiemil/docker/UserAgentRequestHeader.java
@@ -25,10 +25,7 @@
  */
 package com.amihaiemil.docker;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collections;
-import java.util.Properties;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.protocol.RequestDefaultHeaders;
 import org.apache.http.message.BasicHeader;
@@ -41,16 +38,6 @@ import org.apache.http.message.BasicHeader;
 final class UserAgentRequestHeader extends RequestDefaultHeaders {
 
     /**
-     * Config properties file.
-     */
-    private static final String CONFIG_FILE = "config.properties";
-
-    /**
-     * Version property key.
-     */
-    private static final String VERSION_KEY = "build.version";
-
-    /**
      * Ctor.
      */
     UserAgentRequestHeader() {
@@ -60,32 +47,10 @@ final class UserAgentRequestHeader extends RequestDefaultHeaders {
                 String.join(
                     " ",
                     "docker-java-api /",
-                    version(),
+                    "latest",
                     "See https://github.com/amihaiemil/docker-java-api"
                 )
             )
         ));
     }
-
-    /**
-     * Read current version from property file.
-     * @return Build version.
-     */
-    private static String version() {
-        final ClassLoader loader =
-            Thread.currentThread().getContextClassLoader();
-        final String version;
-        final Properties properties = new Properties();
-        try (final InputStream inputStream =
-                 loader.getResourceAsStream(CONFIG_FILE)){
-            properties.load(inputStream);
-            version = properties.getProperty(VERSION_KEY);
-        } catch (final IOException exception) {
-            throw new RuntimeException(
-                String.format("Missing %s file.", CONFIG_FILE)
-            );
-        }
-        return version;
-    }
-
 }
